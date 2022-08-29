a3bf0fef75c1a402ca79031539b7a085b34ba79b
RedJungi <101107394+RedJungi@users.noreply.github.com>
2022-08-29 23:27:18 +0900

    Add files via upload

---
+++ habit-commit/1day/test.py
@@ -0,0 +1,28 @@
+from unittest import result
+
+
+#계산 프로그램의 기본 기능 구현
+
+#더하기 기능 구현
+a=100
+b=50
+result=a+b
+print(result)
+
+#빼기,곱하기,나누기 기능 구현
+
+a=100
+b=50
+result=a+b
+print(result)
+
+print(a,'+',b,'=',result)
+result=a-b
+print(a,'-',b,'=',result)
+result=a*b
+print(a,'*',b,'=',result)
+result=a/b
+print(a,'/',b,'=',result)
+
+
+
