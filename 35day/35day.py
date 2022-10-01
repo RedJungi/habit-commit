5af7d160e71e2870099b361524a1b65dbf9363bf
RedJungi <101107394+RedJungi@users.noreply.github.com>
2022-10-01 21:12:00 +0900

    Add files via upload

---
+++ 35day/35day.py
@@ -0,0 +1,33 @@
+## 함수를 이용한 계산기 ##
+
+# 함수 선언 부분 #
+
+from cgitb import reset
+from unittest import result
+
+
+def calc(v1, v2, op) :
+  result = 0
+  if op == '+' :
+    result = v1 + v2
+  elif op == '=' :
+    result = v1 - v2
+  elif op == '*' :
+    result = v1 * v2
+  elif op == '/' :
+    result = v1 / v2
+
+  return result
+
+  # 전역 변수 선언 부분 #
+  res = 0
+  var1, var2, oper = 0, 0, ""
+
+  # 메인 코드 부분 #
+  oper = input("계산을 입력하세요(+, -, *, /) : ")
+  var1 = int(input("첫 번째 수를 입력하세요 : "))
+  var2 = int(input("두 번째 수를 입력하세요 : "))
+
+  res = calc(var1, var2, oper)
+
+  print("##계산기 : %d %s %d = %d" % (var1, oper, var2, res))
