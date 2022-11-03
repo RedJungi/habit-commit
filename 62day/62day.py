e020e188f40b9cc789edc1f8b6fe32197e29e7dd
RedJungi <101107394+RedJungi@users.noreply.github.com>
2022-11-03 21:10:25 +0900

    Add files via upload

---
+++ 62day/62day.py
@@ -0,0 +1,13 @@
+## 반복문 복습 ##
+
+a = 100
+result = 0
+i = 0
+
+for i in range(1, 5) :
+    result = a << i
+    print("%d << %d = %d" % (a, i, result))
+
+for i in range(1, 5) :
+    result = a >> i
+    print("%d >> %d = %d" % (a, i, result))
